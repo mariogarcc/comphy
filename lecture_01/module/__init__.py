8b312803524ae29fa38ffb356664c66ac2ddf8a0
Mario García <mariogarcc@gmail.com>
2019-02-20 19:09:05 +0100

    Removing unnecessary comments

--- lecture_01/module/__init__.py
+++ lecture_01/module/__init__.py
@@ -1,12 +1,5 @@
 from ..module import *
 
-# from .bisect import *
-# from .find_sols import *
-# from .sign_changes import *
-# from .regula_falsi import *
-# from .newrap_sol import *
-# from .ask import *
-
 __all__ = [
     'find_sols',
     'bis_solve',
--- lecture_02/module/__init__.py
+++ lecture_02/module/__init__.py
@@ -1,9 +1,5 @@
 from ..module import *
 
-# from .find_sols import *
-# from .bisect import *
-# from .ask import *
-
 __all__ = [
     'find_sols',
     'bis_solve',
