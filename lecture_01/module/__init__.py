ce3a128fbee80c5bbc66dc16d4a883b34260d5ab
Mario García <mariogarcc@gmail.com>
2019-03-02 12:32:44 +0100

    Removing module subfolders
    They're unnecessary - we're able to import package
    from the main `.py` files using the `-m` flag anyway

--- lecture_01/module/__init__.py
+++
@@ -1,9 +0,0 @@
-from ..package import *
-
-__all__ = [
-    'find_sols',
-    'bisect_solve',
-    'falsi_solve',
-    'newt_raph_solve',
-    'ask_continue',
-]
--- lecture_02/module/__init__.py
+++
@@ -1,7 +0,0 @@
-from ..package import *
-
-__all__ = [
-    'find_sols',
-    'bisect_solve',
-    'ask_continue',
-]
