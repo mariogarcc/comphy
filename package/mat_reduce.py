7e1d995523ab57468783f7450d8715937e941b3c
Mario García <mariogarcc@gmail.com>
2019-03-02 18:09:33 +0100

    inplace kwarg

--- package/mat_reduce.py
+++ package/mat_reduce.py
@@ -4,12 +4,12 @@ import numpy as np
 from .solve_algebra import solve_triang_mat
 from .mat_ops import mat_shift
 
-def gauss_solve(mat, pivoting = 'none', method = 'g-elim', shift = 0):
+def gauss_reduce(mat, method = 'g-elim', pivoting = 'none',
+    shift = 0, inplace = False):
     """
     Solves an extended matrix corresponding to a linear system of
     equations.
     """
-    cmat = copy.deepcopy(mat)
 
     # check args
     pivoting = pivoting.lower()
@@ -25,20 +25,24 @@ def gauss_solve(mat, pivoting = 'none', method = 'g-elim', shift = 0):
         if any([all([coeff for coeff in mat[:,c] == 0]) \
                 for c in range(mat.shape[1]-1)]) == True \
             or mat.shape[1] != mat.shape[0]+1:
-            raise ValueError("{!r} is not a valid matrix".format(mat))
+            raise ValueError("{!r}\nis not a valid matrix".format(mat))
     except AttributeError:
-        raise ValueError("{!r} is not a valid matrix".format(mat))
+        raise ValueError("{!r}\nis not a valid matrix".format(mat))
 
-    neqs = mat.shape[0]
+    if inplace:
+        cmat = mat
+    else:
+        cmat = copy.deepcopy(mat)
+
+    neqs = cmat.shape[0]
     for i in range(neqs-1):
-        print(i)
 
         if pivoting == 'partial':
 
             for r in range(i+1, neqs):
                 rvals = []
-                if abs(mat[r,i]) > abs(mat[i,i]):
-                    rvals.append([mat[r,i], r])
+                if abs(cmat[r,i]) > abs(cmat[i,i]):
+                    rvals.append([cmat[r,i], r])
             if rvals:
                 if len(rvals) == 1:
                     nr = rvals[0][1]
@@ -48,44 +52,34 @@ def gauss_solve(mat, pivoting = 'none', method = 'g-elim', shift = 0):
                     except:
                         nr = rvals[0]
 
-                temp_mat = copy.deepcopy(mat)
-                mat[i,], mat[nr,] = temp_mat[nr,], temp_mat[i,]
+                temp_mat = copy.deepcopy(cmat)
+                cmat[i,], cmat[nr,] = temp_mat[nr,], temp_mat[i,]
             
         if pivoting == 'total':
 
             # get the indices of the maximum absolute value
             temp_max_loc = np.unravel_index( \
-                abs(mat[i:,i:-1]).argmax(), mat[i:,i:-1].shape)
+                abs(cmat[i:,i:-1]).argmax(), cmat[i:,i:-1].shape)
             max_loc = (temp_max_loc[0]+i, temp_max_loc[1]+i)
 
             # make the maximum absolute value the pivoting element
-            temp_mat = copy.deepcopy(mat)
-            mat[i,:], mat[max_loc[0],:] = temp_mat[max_loc[0],:], temp_mat[i,:]
-            temp_mat = copy.deepcopy(mat)
-            mat[:,i], mat[:,max_loc[1]] = temp_mat[:,max_loc[1]], temp_mat[:,i]
+            temp_mat = copy.deepcopy(cmat)
+            cmat[i,:], cmat[max_loc[0],:] = temp_mat[max_loc[0],:], temp_mat[i,:]
+            temp_mat = copy.deepcopy(cmat)
+            cmat[:,i], cmat[:,max_loc[1]] = temp_mat[:,max_loc[1]], temp_mat[:,i]
 
         for eq in range(i+1, neqs):
-            print(eq)
             # i = 0 -> eq = 1, 2
-            # i = 1 -> eq = 2 ... (for this case, where size = 3)
+            # i = 1 -> eq = 2 ... (for a case where size = 3)
             with np.errstate(divide = 'raise', invalid = 'raise'):
                 try:
-                    mat[eq,:] -= mat[i,:] * mat[eq,i]/mat[i,i]
+                    cmat[eq,:] -= cmat[i,:] * cmat[eq,i]/cmat[i,i]
                 except(ZeroDivisionError, FloatingPointError):
                     if shift > mat.shape[0]-1:
-                        raise ValueError("cannot solve matrix")
-                    gauss_solve(mat_shift(cmat), pivoting = pivoting, 
+                        raise ValueError("cannot reduce matrix")
+                    gauss_reduce(mat_shift(mat), pivoting = pivoting, 
                         shift = shift+1)
-                    # shifting is only a partial solution - the system could
-                    # still be solved as long as all the coefficients in a col
-                    # corresponding to each variable are not zero, but this
-                    # method will only allow up to *neqs* permutations instead
-                    # of *neqs!*, which would eventually yield the solutions
-                    # as long as they're obtainable
+                    # shifting is only a partial solution
+                    # there are neqs! permutations, but that's too many to try
 
-                    # someone could also use itertools.permutations()
-                    # https://docs.python.org/3.7/library/itertools.html#itertools.permutations
-
-    print(mat)
-    return solve_triang_mat(mat)
-
+    return cmat
