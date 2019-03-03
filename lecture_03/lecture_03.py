888687a3d5d7f4a6617e3ec25accdd23b8917985
Mario García <mariogarcc@gmail.com>
2019-03-03 21:38:30 +0100

    Clearing

--- lecture_03/lecture_03.py
+++ lecture_03/lecture_03.py
@@ -26,11 +26,13 @@ ab = np.concatenate((a, b), axis = 1)
 print("Matrix of coefficients A (given):", a, sep = '\n\n', end = '\n\n')
 print("Vector of independent terms B (given):", b, sep = '\n\n', end = '\n\n')
 print("Expanded matrix (AB):", ab, sep = '\n\n', end = '\n\n')
-tmat = gauss_reduce(ab)
+tmat = gauss_reduce(ab, pivoting = 'none')
 print("Triangularized matrix (T):", tmat, sep = '\n\n', end = '\n\n')
 sols = solve_triang_mat(tmat)
 print("Obtained solutions:", sols, sep = '\n\n', end = '\n\n')
 osols = check_sys_sols(sols, ab)
+if osols is None:
+    print("Obtained solutions were incorrect.")
 print("Ordered solutions:", osols, sep = '\n\n', end = '\n\n')
 print("i.e.:", *("x_{i} = {sol_i}"
     .format(i = i+1, sol_i = osols[i]) for i in range(len(osols))),
