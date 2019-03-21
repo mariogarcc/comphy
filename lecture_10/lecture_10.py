6e804546d7a4acc8142eeeb91be16070c53808d1
Mario García <mariogarcc@gmail.com>
2019-03-21 13:25:38 +0100

    Including integral error in Lecture 10 exercise

--- lecture_10/lecture_10.py
+++ lecture_10/lecture_10.py
@@ -106,11 +106,12 @@ redact_ex(EXERCISE_23, 23)
 npoints = 1e6
 n = int(npoints)
 
-l, h = 8, 6 # figure specs
+l, h = 8, 6 # rectangle of width 8 and height 6
 # empty circle is located at (6, 0) and has radius of 2
 xc, yc, rc = 6, 0, 2
 
 xv, yv = 0, 0
+xerr, yerr = 0, 0
 # xp, yp = [], []
 inc = 0
 for i in range(n):
@@ -122,6 +123,8 @@ for i in range(n):
     inc += 1
     xv += x
     yv += y
+    xerr += x**2
+    yerr += y**2
     # xp.append(x)
     # yp.append(y)
     # if i % int(np.sqrt(n)) == 0:
@@ -132,11 +135,24 @@ for i in range(n):
 # plt.xlim(0, 8)
 # plt.show()
 
-xg = xv/inc
-yg = yv/inc
+# xg = xv/inc
+# yg = yv/inc
+# or
+
+v = n/inc
+
+xmf = xv/n; xmf2 = xerr/n
+ymf = yv/n; ymf2 = yerr/n
+
+xg = v*xmf
+yg = v*ymf
+
+xgerr = v*np.sqrt((xmf2-xmf**2)/n)
+ygerr = v*np.sqrt((ymf2-ymf**2)/n)
 
 print("Obtained coordinates for center of mass (x, y):",
-    "({:.6f}, {:.6f})".format(xg, yg), sep = '\n', end = '\n\n')
+    "({:.6f} \u00B1 {:.6f}, {:.6f} \u00B1 {:.6f})"
+    .format(xg, xgerr, yg, ygerr), sep = '\n', end = '\n\n')
 
 
 EXERCISE_24 = """\
