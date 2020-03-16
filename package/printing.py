6a76d74c0c85f868b202a091bcca77b3cdc0a5b1
mariogarcc <mariogarcc@gmail.com>
2020-03-16 18:27:03 +0100

    stuff

--- package/printing.py
+++ package/printing.py
@@ -1,5 +1,5 @@
 def redact_ex(ex_str, n = '', sep = '\n', end = '\n\n'):
-    print('', 'EXERCISE {no:d}:'.format(
+    print('', 'EXERCISE {no}:'.format(
         no = int(n) if not isinstance(n, str) else n), ex_str,
         sep = sep, end = end)
     return None
